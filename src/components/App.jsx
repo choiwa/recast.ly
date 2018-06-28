d6490b5b35f9b90229e798674aa7b6d846f0940f
choiwa <joyjoycho@gmail.com>
2018-06-27 19:14:40 -0700

    Implemented VideoList showing different entries based on example Data.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-var App = () => (
+var App = (props) => (
   <div>
     <nav className="navbar">
       <div className="col-md-6 offset-md-3">
@@ -6,11 +6,11 @@ var App = () => (
       </div>
     </nav>
     <div className="row">
-      <div className="col-md-7">
+      <div className="col-md-7"> 
         <div><h5><em>videoPlayer</em> view goes here</h5></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        <div><h5><VideoList videos = {props.videos}/></h5></div>
       </div>
     </div>
   </div>
