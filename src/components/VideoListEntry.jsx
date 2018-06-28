fc740df543b4f50571f02826bc9c34dc6c67cb86
choiwa <joyjoycho@gmail.com>
2018-06-27 19:29:12 -0700

    fixed bug on the image tag

--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -4,12 +4,13 @@ class VideoListEntry extends React.Component {
     super(props);
   
   }
+
   
   render () {
     return(
       <div className="video-list-entry media">
         <div className="media-left media-middle">
-          <img className="media-object" src='{this.props.video.snippet.thumbnails.default.url}' alt="" />
+          <img className="media-object" src={this.props.video.snippet.thumbnails.default.url} alt="" />
         </div>
         <div className="media-body">
           <div className="video-list-entry-title"> {this.props.video.snippet.title} </div>
